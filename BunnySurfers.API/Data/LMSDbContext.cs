507f31df7e11ab1e703b90f6b664ce12164e5a41
Jareth Holt <jareth.holt@gmail.com>
2024-06-18 10:54:23 +0200

    feat: Set up database creation
    - Add required NuGet packages
    - Create a DbContext class (currently empty)
    - Configure a database to use in Program with database name defined in appsettings.ConnectionStrings

---
+++ BunnySurfers.API/Data/LMSDbContext.cs
@@ -0,0 +1,9 @@
+﻿using BunnySurfers.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BunnySurfers.API.Data
+{
+    public class LMSDbContext(DbContextOptions<LMSDbContext> options) : DbContext(options)
+    {
+    }
+}
