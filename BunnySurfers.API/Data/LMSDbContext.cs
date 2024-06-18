0c2b7d740edbd2f90b71537cfb63e5209aad26bb
Jareth Holt <jareth.holt@gmail.com>
2024-06-18 11:10:51 +0200

    feat: Add initial migration
    Add the Initial migration for the database. Add each of the entities to the LMSDbContext class and explicitly create Student and Teacher tables in the OnModelCreating method.

--- BunnySurfers.API/Data/LMSDbContext.cs
+++ BunnySurfers.API/Data/LMSDbContext.cs
@@ -5,5 +5,40 @@ namespace BunnySurfers.API.Data
 {
     public class LMSDbContext(DbContextOptions<LMSDbContext> options) : DbContext(options)
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Document> Documents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*
+             * Without any additional configuration, add-migration correctly creates:
+             * - Courses
+             * - Modules
+             * - Activities
+             * - Courses x Teachers
+             * It incorrectly creates:
+             * - Users: Additional CourseId column; maybe unnecessary Discriminator column
+             * - Documents: Additional nullable columns for Course/Module/Activity Ids
+             * It fails to create:
+             * - Students
+             * - Teachers
+             */
+
+            // Establish Students table
+            modelBuilder.Entity<Student>()
+                .ToTable("Students");
+
+            // Establish Teachers table
+            modelBuilder.Entity<Teacher>()
+                .ToTable("Teachers");
+
+            // Explicitly creating these two tables fixes the above issues
+            // For Documents the extra nullable columns are left in
+        }
     }
 }
