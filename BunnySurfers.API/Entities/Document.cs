7619267be9c746505ef2bf96556073206bc8091c
Jareth Holt <jareth.holt@gmail.com>
2024-06-18 09:39:40 +0200

    feat: Add Document class
    Add the Document class and lists of documents for all classes.
    
    NB: I add the list of documents to the base User class instead of the derived Student or Teacher classes. We should keep tabs on whether this causes any issues.

---
+++ BunnySurfers.API/Entities/Document.cs
@@ -0,0 +1,15 @@
+﻿namespace BunnySurfers.API.Entities
+{
+    public class Document
+    {
+        // The actual document can be accessed through a separate table
+        // with the document filepath linked to this DocumentId
+        public int DocumentId { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateTime TimeOfUpload { get; set; }
+
+        // Which user uploaded the document?
+        public User UploadedBy { get; set; } = null!;
+    }
+}
--- BunnySurfers.API/Entities/User.cs
+++ BunnySurfers.API/Entities/User.cs
@@ -5,5 +5,8 @@
         public int UserId { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
+
+        // All users can upload documents
+        public List<Document> Documents { get; set; } = [];
     }
 }
