d40137786939f10fbc61468978c229f4eeb5a1b7
Jareth Holt <jareth.holt@gmail.com>
2024-06-18 09:31:33 +0200

    feat: Add the Course and related classes
    Add the following classes:
    - Course: contains many students, (possibly) many teachers, and many Modules
    - Module: belongs to one course, contains many Activities
    - Activity: belongs to one module, has an ActivityType
    - ActivityType: enum of activity categories

---
+++ BunnySurfers.API/Entities/Activity.cs
@@ -0,0 +1,18 @@
+﻿namespace BunnySurfers.API.Entities
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public ActivityType ActivityType { get; set; }
+
+        // Activities may occur at certain times, not just dates
+        public DateTime StartDate { get; set; }
+        // Not all activities have two dates, e.g. due date of an assignment
+        public DateTime? EndDate { get; set; }
+
+        // An activity belongs to one module
+        public Module Module { get; set; } = null!;
+    }
+}
---
+++ BunnySurfers.API/Entities/Module.cs
@@ -0,0 +1,17 @@
+﻿namespace BunnySurfers.API.Entities
+{
+    public class Module
+    {
+        public int ModuleId { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+        // A module belongs to a single course
+        public Course Course { get; set; } = null!;
+
+        // A module is composed of many activities
+        public List<Activity> Acitivities { get; set; } = [];
+    }
+}
