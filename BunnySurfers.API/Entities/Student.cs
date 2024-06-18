20775f1a3741e299dac08e1007f3862aae8c82c0
Jareth Holt <jareth.holt@gmail.com>
2024-06-18 09:17:10 +0200

    feat: Add User classes
    Add the User base class and Student and Teacher derived classes.

---
+++ BunnySurfers.API/Entities/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace BunnySurfers.API.Entities
+{
+    public class Student : User
+    {
+        // The Course is nullable in case the student is not currently enrolled in anything
+        public int? CourseId { get; set; }
+        public Course? Course { get; set; }
+    }
+}
