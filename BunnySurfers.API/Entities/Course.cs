82722f3df07d0f20f44b4a10a0c2e9e9fb57232f
Jareth Holt <jareth.holt@gmail.com>
2024-06-19 12:58:07 +0200

    refactor: Remove Student and Teacher classes
    Remove the Student and Teacher entity classes, treating both as their base class User. The constraint that Students can only have one Course while teachers can have many must be enforced elsewhere in the application.

--- BunnySurfers.API/Entities/Course.cs
+++ BunnySurfers.API/Entities/Course.cs
@@ -9,10 +9,8 @@
         public DateOnly EndDate { get; set; }
 
         // Courses have many students
-        public List<Student> Students { get; set; } = [];
-
         // Courses may have many teachers?
-        public List<Teacher> Teachers { get; set; } = [];
+        public List<User> Users { get; set; } = [];
 
         // Courses are composed of many modules
         public List<Module> Modules { get; set; } = [];
--- BunnySurfers.API/Entities/Teacher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BunnySurfers.API.Entities
-{
-    public class Teacher : User
-    {
-        // Teachers can have many courses; courses may have many teachers?
-        public List<Course> Courses { get; set; } = [];
-    }
-}
