a5aabd174117dcd250fa3b842fdd643888e692ab
Jareth Holt <jareth.holt@gmail.com>
2024-06-20 11:46:04 +0200

    feat: Add GetParticipants to StudentController
    - Add GetParticipants to retrieve all participants for the course this student is enrolled in
    - Remove the CheckStudentExists method since the database calls were being duplicated anyway
    - Add check in GetCourse that the found user is a student

--- BunnySurfers.API/Controllers/StudentController.cs
+++ BunnySurfers.API/Controllers/StudentController.cs
@@ -11,27 +11,38 @@ namespace BunnySurfers.API.Controllers
     {
         private readonly LMSDbContext _context = context;
 
-        // Check that a user exists and is a student
-        private bool CheckStudentExists(int userId)
-        {
-            var user = _context.Users.SingleOrDefault(x => x.UserId == userId);
-            return (user is not null && user.Role == UserRole.Student);
-        }
-
         // Get the course this student is in, if any
         [HttpGet("course")]
         public async Task<ActionResult<Course?>> GetCourse(int userId)
         {
-            if (!CheckStudentExists(userId))
-                return BadRequest();
-
-            var _student = await _context.Users.Include(x => x.Courses).FirstOrDefaultAsync(x => x.UserId == userId);
-            if (_student is null)
-                return BadRequest();
-            User student = _student;
+            var student = await _context.Users
+                .Include(u => u.Courses)
+                .SingleOrDefaultAsync(u => u.UserId == userId);
+            if (student is null)
+                return NotFound();
+            if (student.Role != UserRole.Student)
+                return BadRequest($"The user with ID {userId} is not a student");
 
             var course = student.Courses.SingleOrDefault();
             return Ok(course);
+        }
+
+        // Get the participants of the student's course
+        [HttpGet("course/participants")]
+        public async Task<ActionResult<IEnumerable<User>>> GetParticipants(int userId)
+        {
+            var student = await _context.Users
+                .Include(u => u.Courses)
+                .ThenInclude(c => c.Users)
+                .SingleOrDefaultAsync(u => u.UserId == userId);
+            if (student is null)
+                return NotFound();
+            if (student.Role != UserRole.Student)
+                return BadRequest($"The user with ID {userId} is not a student");
+
+            var course = student.Courses.SingleOrDefault();
+            var participants = (course is null) ? Enumerable.Empty<User>() : course.Users;
+            return Ok(participants);
         }
     }
 }
