a960250d23eed1f6aa3686aebb3babaa56f6d55d
Jareth Holt <jareth.holt@gmail.com>
2024-06-21 12:34:38 +0200

    feat: Add DatabaseController with Reset action
    Add a simple DatabaseController with one action, ResetDatabase, which deletes and creates the database.

---
+++ BunnySurfers.API/Controllers/DatabaseController.cs
@@ -0,0 +1,21 @@
+﻿using BunnySurfers.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BunnySurfers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController(LMSDbContext context) : ControllerBase
+    {
+        private readonly LMSDbContext _context = context;
+
+        // POST: Reset database with seed data
+        [HttpPost]
+        public async Task<IActionResult> ResetDatabase()
+        {
+            await _context.Database.EnsureDeletedAsync();
+            await _context.Database.EnsureCreatedAsync();
+            return NoContent();
+        }
+    }
+}
