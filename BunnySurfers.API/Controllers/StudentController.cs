6d1dcdbdc58430c0e439b388a11fe4004f4c0924
Jareth Holt <jareth.holt@gmail.com>
2024-06-25 15:15:19 +0200

    feat: Add Get method for modules to StudentController
    Misc: Fix the users added to the first course in SeedData so that the Admin and None roles are left out.

--- BunnySurfers.API/Controllers/StudentController.cs
+++ BunnySurfers.API/Controllers/StudentController.cs
@@ -20,12 +20,32 @@ namespace BunnySurfers.API.Controllers
                 .ThenInclude(c => c.Users)
                 .SingleOrDefaultAsync(u => u.UserId == userId);
             if (student is null)
-                return NotFound();
+                return NotFound($"A user with ID {userId} could not be found");
             if (student.Role != UserRole.Student)
                 return BadRequest($"The user with ID {userId} is not a student");
 
             var course = student.Courses.SingleOrDefault();
             return Ok(course);
+        }
+
+        // Get the modules for the course the student is in
+        [HttpGet("modules")]
+        public async Task<ActionResult<IEnumerable<Module>>> GetModules(int userId)
+        {
+            var student = await _context.Users
+                .Include(u => u.Courses)
+                .ThenInclude(c => c.Modules)
+                .SingleOrDefaultAsync(u => u.UserId == userId);
+            if (student is null)
+                return NotFound($"A user with ID {userId} could not be found");
+            if (student.Role != UserRole.Student)
+                return BadRequest($"The user with ID {userId} is not a student");
+
+            var course = student.Courses.SingleOrDefault();
+            if (course is null)
+                return BadRequest($"The student with ID {userId} is not currently enrolled in a course");
+
+            return Ok(course.Modules);
         }
     }
 }
