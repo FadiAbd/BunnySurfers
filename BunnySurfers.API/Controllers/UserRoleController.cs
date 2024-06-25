0c84beb5936fb114dd02c356b17d0a63bb2f5d21
Jareth Holt <jareth.holt@gmail.com>
2024-06-25 13:07:05 +0200

    feat: Add controller for viewing and changing UserRole
    - Add UserRoleController with Get and Put methods, both of which deal with a single user
    - Enable JSON enum converter in Program
    - Add "Admin" to UserRole enum
    - Add an Admin to SeedData

---
+++ BunnySurfers.API/Controllers/UserRoleController.cs
@@ -0,0 +1,53 @@
+﻿using BunnySurfers.API.Data;
+using BunnySurfers.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BunnySurfers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRoleController(LMSDbContext context) : ControllerBase
+    {
+        private readonly LMSDbContext _context = context;
+
+        // Get the role of a given user
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult<UserRole>> GetRole(int userId)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(u => u.UserId == userId);
+            if (user is null)
+                return NotFound($"No user found with ID {userId}");
+            else
+                return Ok(user.Role);
+        }
+
+        // Change the role of a given user
+        [HttpPut("{userId:int}")]
+        public async Task<IActionResult> PutRole(int userId, UserRole role)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(u => u.UserId == userId);
+            if (user is null)
+                return NotFound($"No user found with ID {userId}");
+
+            user.Role = role;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(userId))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return NoContent();
+        }
+
+        private bool UserExists(int userId) =>
+            _context.Users.Any(u => u.UserId == userId);
+    }
+}
