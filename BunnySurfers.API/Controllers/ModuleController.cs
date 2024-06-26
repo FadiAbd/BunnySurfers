b4ccde319b97933c0ff6d8a24aee190fd2f9e721
Jareth Holt <jareth.holt@gmail.com>
2024-06-26 09:35:01 +0200

    feat: Add Put method in ModuleController
    Add a Put method to the ModuleController using the ModuleForPostDTO. This means that only the name, description, start and end dates, and course ID of a module can be changed through the controller.

--- BunnySurfers.API/Controllers/ModuleController.cs
+++ BunnySurfers.API/Controllers/ModuleController.cs
@@ -3,6 +3,7 @@ using BunnySurfers.API.Data;
 using BunnySurfers.API.DTOs;
 using BunnySurfers.API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BunnySurfers.API.Controllers
 {
@@ -44,6 +45,34 @@ namespace BunnySurfers.API.Controllers
 
             _context.Modules.Remove(module);
             await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{moduleId:int}")]
+        public async Task<IActionResult> PutModule(int moduleId, ModuleForPostDTO moduleDTO)
+        {
+            var module = await _context.Modules.FindAsync(moduleId);
+            if (module is null)
+                return NotFound($"No module with ID {moduleId} was found");
+
+            // Update module from moduleDTO
+            module.Name = moduleDTO.Name;
+            module.Description = moduleDTO.Description;
+            module.StartDate = moduleDTO.StartDate;
+            module.EndDate = moduleDTO.EndDate;
+            module.CourseId = moduleDTO.CourseId;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Modules.Find(moduleId) is null)
+                    return NotFound($"The module with ID {moduleId} is missing");
+                else
+                    throw;
+            }
             return NoContent();
         }
     }
