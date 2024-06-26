6ee9402ac10993ae8db74a8dae9ffb0053fe3622
Jareth Holt <jareth.holt@gmail.com>
2024-06-26 15:44:06 +0200

    refactor: Use DTOs to combine queries for StudentViewController
    - Rename StudentController to StudentViewController to make it clearer that this is not the controller for editing students but the one that students will access
    - Add DTO classes for the StudentView that nest within each other, so that the highest-level Course DTO is still non-recursive
    - Expand the top-level HttpGet method to include in the students' Course the other Users, Modules, and Activities
    - Remove the other Get methods in the controller since they are now redundant

---
+++ BunnySurfers.API/Controllers/StudentViewController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using BunnySurfers.API.Data;
+using BunnySurfers.API.DTOs;
+using BunnySurfers.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BunnySurfers.API.Controllers
+{
+    [Route("api/[controller]/{studentId:int}")]
+    [ApiController]
+    public class StudentViewController(LMSDbContext context, IMapper mapper) : ControllerBase
+    {
+        private readonly LMSDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        // Get the course this student is in, if any
+        [HttpGet]
+        public async Task<ActionResult<CourseForStudentViewDTO?>> GetCourse(int studentId)
+        {
+            var student = await _context.Users
+                .Include(u => u.Documents)
+                .Include(u => u.Courses)
+                .ThenInclude(c => c.Users)
+                .Include(u => u.Courses)
+                .ThenInclude(c => c.Modules)
+                .ThenInclude(m => m.Activities)
+                .FirstOrDefaultAsync(u => u.UserId == studentId);
+            if (student is null)
+                return NotFound($"A user with ID {studentId} could not be found");
+            if (student.Role != UserRole.Student)
+                return BadRequest($"The user with ID {studentId} is not a student");
+
+            var course = student.Courses.FirstOrDefault();
+            var courseDTO = (course is null) ? null : _mapper.Map<CourseForStudentViewDTO>(course);
+            return Ok(courseDTO);
+        }
+    }
+}
---
+++ BunnySurfers.API/DTOs/StudentViewDTOs.cs
@@ -0,0 +1,62 @@
+﻿using BunnySurfers.API.Entities;
+
+namespace BunnySurfers.API.DTOs
+{
+    public class UserForStudentViewDTO
+    {
+        public int UserId { get; set; }
+        public UserRole Role { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+
+    public class DocumentForStudentViewDTO
+    {
+        public int DocumentId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime TimeOfUpload { get; set; }
+        public required string UntrustedFileName { get; set; }
+
+        public UserForStudentViewDTO UploadedBy { get; set; } = null!;
+    }
+
+    public class ActivityForStudentViewDTO
+    {
+        public int ActivityId { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public ActivityType ActivityType { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int ModuleId { get; set; }
+
+        public List<DocumentForStudentViewDTO> Documents { get; set; } = [];
+    }
+
+    public class ModuleForStudentViewDTO
+    {
+        public int ModuleId { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public int CourseId { get; set; }
+
+        public List<ActivityForStudentViewDTO> Activities { get; set; } = [];
+        public List<DocumentForStudentViewDTO> Documents { get; set; } = [];
+    }
+
+    public class CourseForStudentViewDTO
+    {
+        public int CourseId { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+        public List<UserForStudentViewDTO> Users { get; set; } = [];
+        public List<ModuleForStudentViewDTO> Modules { get; set; } = [];
+        public List<DocumentForStudentViewDTO> Documents { get; set; } = [];
+    }
+}
