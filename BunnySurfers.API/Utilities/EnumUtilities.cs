3b1fb0c922e0bf463eeb10e55a3a6c042c811699
Jareth Holt <jareth.holt@gmail.com>
2024-06-27 15:44:09 +0200

    feat: Add UserRoleDropdown component
    Add a component for providing a dropdown menu with the UserRole enum. Create a simple UserManagement page to test that it works.

--- BunnySurfers.API/Utilities/EnumUtilities.cs
+++ BunnySurfers.API/Utilities/EnumUtilities.cs
@@ -12,5 +12,16 @@
                 valueStrings[i] = $"{names[i]} ({values[i]})";
             return string.Join("; ", valueStrings);
         }
+
+        public static Dictionary<int, string> ConvertToDict<TEnum>() where TEnum : struct, Enum
+        {
+            var names = Enum.GetNames<TEnum>();
+            var values = (int[])Enum.GetValuesAsUnderlyingType<TEnum>();
+            var numValues = names.Length;
+            Dictionary<int, string> enumAsDict = [];
+            for (int i = 0; i < numValues; i++)
+                enumAsDict[values[i]] = names[i];
+            return enumAsDict;
+        }
     }
 }
---
+++ BunnySurfers.Blazor/Components/Pages/UserManagement.razor.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using BunnySurfers.API.DTOs;
+using BunnySurfers.API.Entities;
+using BunnySurfers.API.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BunnySurfers.Blazor.Components.Pages
+{
+    public partial class UserManagement
+    {
+        [Inject]
+        public HttpClient ApiClient { get; set; } = null!;
+
+        [Parameter]
+        public int UserId { get; set; }
+
+        public UserEditDTO? UserDTO { get; set; } = null;
+
+        protected bool getUserError;
+        protected bool shouldRender;
+
+        protected override async Task OnInitializedAsync()
+        {
+            // Set up and execute the API call
+            var request = new HttpRequestMessage(HttpMethod.Get, $"api/Users/{UserId}");
+            var response = await ApiClient.SendAsync(request);
+
+            // If the API call is unsuccessful, return now
+            if (!response.IsSuccessStatusCode)
+            {
+                getUserError = true;
+                shouldRender = true;
+                return;
+            }
+
+            // Parse the API response into a User
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            UserDTO = await JsonSerializer.DeserializeAsync<UserGetDTO?>(responseStream);
+            shouldRender = true;
+        }
+
+        protected void OnUserRoleChanged(int selectedRoleInt)
+        {
+            UserDTO!.Role = (UserRole)selectedRoleInt;
+        }
+
+        protected async Task OnSubmit()
+        {
+            // Set up and execute the API call
+            var response = await ApiClient.PutAsJsonAsync($"api/Users/{UserId}", UserDTO);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
