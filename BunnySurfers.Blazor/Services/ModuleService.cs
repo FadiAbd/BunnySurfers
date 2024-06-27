2bab123a8ce3e4da3fa36af40c9902372ac82f5c
fadi abdallah <fadishadow@hotmail.com>
2024-06-27 15:15:26 +0200

    Added courseId to EditCourse Navigation

---
+++ BunnySurfers.Blazor/Services/ModuleService.cs
@@ -0,0 +1,32 @@
+﻿using BunnySurfers.API.Entities;
+
+namespace BunnySurfers.Blazor.Services
+{
+    public class ModuleService : IModuleService
+    {
+        public Task CreateModule(Module module)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteModule(int moduleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Module> GetModuleById(int moduleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Module>> GetModules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateModule(int moduleId, Module module)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
