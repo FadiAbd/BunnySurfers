4d1d846b5c9dc94d6bc5993f29be0bae9da2c75f
Olle Tengnér <olle.code@hotmail.com>
2024-06-28 14:48:09 +0200

    first

---
+++ BunnySurfers.Blazor/Services/UserGetDTOs.cs
@@ -0,0 +1,11 @@
+﻿namespace BunnySurfers.Blazor.Services
+{
+
+
+    public class UserGetDTOs
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string UserId { get; set; }
+    }
+}
