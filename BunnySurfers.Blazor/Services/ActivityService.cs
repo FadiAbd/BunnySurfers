804f62fbd94bf3dd5c4d99eaed190340098ae54b
fadi abdallah <fadishadow@hotmail.com>
2024-07-02 12:54:53 +0200

    CreateActivities front end

---
+++ BunnySurfers.Blazor/Services/ActivityService.cs
@@ -0,0 +1,41 @@
+﻿using BunnySurfers.API.Entities;
+
+namespace BunnySurfers.Blazor.Services
+{
+    public class ActivityService : IActivityService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ActivityService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task CreateActivity(Activity activity)
+        {
+             var response = await _httpClient.PostAsJsonAsync($"api/Activity", activity);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteActivity(int activityId)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Activity/{activityId}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<List<Activity>> GetActivities()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Activity>>("api/Activity");
+        }
+
+        public async Task<Activity> GetActivityById(int activityId)
+        {
+            return await _httpClient.GetFromJsonAsync<Activity>($"api/Activity/{activityId}");
+        }
+
+        public async Task UpdateActivity(int activityId, Activity activityyew)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Activity/ {activityId}", activityyew);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ BunnySurfers.Blazor/Services/IActivityService.cs
@@ -0,0 +1,13 @@
+﻿using BunnySurfers.API.Entities;
+
+namespace BunnySurfers.Blazor.Services
+{
+    public interface IActivityService
+    {
+        Task<List<Activity>> GetActivities();
+        Task<Activity> GetActivityById(int activityId);
+        Task CreateActivity(Activity activity);
+        Task UpdateActivity(int activityId, Activity activityyew);
+        Task DeleteActivity(int activityId);
+    }
+}
